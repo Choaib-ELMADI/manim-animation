acbc298406b07b3c66b5a469359bff7c4ff79e80
Choaib ELMADI <choaibamd@gmail.com>
2024-11-10 14:48:46 +0100

    Create plot_step_function method

--- Projects/Communication_Protocols/UART/main.py
+++ Projects/Communication_Protocols/UART/main.py
@@ -2,39 +2,9 @@ from manim import *
 
 
 class Main(Scene):
-    def construct(self):
-        #            .  .  s  d_____a________t_____a  e  .  .
-        data_bits = [1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1]
+    def plot_step_function(self, data_bits, axe):
         is_transmitting = False
         sent_bits_counter = 0
-
-        axe = (
-            Axes(
-                x_range=[0, 14, 1],
-                y_range=[0, 2, 1],
-                x_length=21,
-                y_length=4,
-                axis_config={"tip_shape": StealthTip, "tip_length": 0.1},
-            )
-            .add_coordinates()
-            .scale(0.5)
-        )
-
-        borders = SurroundingRectangle(
-            axe, color="PURPLE", fill_opacity=0.1, fill_color="PURPLE", buff=0.25
-        )
-
-        title = (
-            Text("UART Communication Protocol", color="RED")
-            .scale(0.5)
-            .next_to(axe, UP)
-            .shift(UP * 0.25)
-        )
-
-        self.wait(0.25)
-        # self.play(Create(axe))
-        self.add(axe)
-        self.wait(1)
 
         start_x = 0
         start_y = data_bits[0]
@@ -61,10 +31,10 @@ class Main(Scene):
                 .scale(0.5)
                 .next_to(line, UP, buff=0.1)
             )
-            # self.play(Create(line), run_time=0.5)
-            self.add(line)
-            # self.play(Write(curr_bit_text))
-            self.add(curr_bit_text)
+            self.play(Create(line), run_time=0.5)
+            # self.add(line)
+            self.play(Write(curr_bit_text))
+            # self.add(curr_bit_text)
 
             #! TRANSITION BETWEEN 1 AND 0
             if i < len(data_bits) - 1 and data_bits[i + 1] != bit:
@@ -73,16 +43,50 @@ class Main(Scene):
                     axe.c2p(end_x, data_bits[i + 1]),
                     color="GREEN",
                 )
-                # self.play(Create(vert_line), run_time=0.25)
-                self.add(vert_line)
+                self.play(Create(vert_line), run_time=0.25)
+                # self.add(vert_line)
 
             start_x = end_x
             start_y = data_bits[i + 1] if i < len(data_bits) - 1 else start_y
 
+    def construct(self):
+        #            .  .  s  d_____a________t_____a  e  .  .
+        data_bits = [1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1]
+
+        axe = (
+            Axes(
+                x_range=[0, 14, 1],
+                y_range=[0, 2, 1],
+                x_length=21,
+                y_length=4,
+                axis_config={"tip_shape": StealthTip, "tip_length": 0.1},
+            )
+            .add_coordinates()
+            .scale(0.5)
+        )
+
+        borders = SurroundingRectangle(
+            axe, color="PURPLE", fill_opacity=0.1, fill_color="PURPLE", buff=0.25
+        )
+
+        title = (
+            Text("UART Communication Protocol", color="RED")
+            .scale(0.5)
+            .next_to(axe, UP)
+            .shift(UP * 0.25)
+        )
+
+        self.wait(0.25)
+        self.play(Create(axe))
+        # self.add(axe)
         self.wait(1)
-        # self.play(DrawBorderThenFill(borders))
-        self.add(borders)
+
+        self.plot_step_function(data_bits, axe)
+
         self.wait(1)
-        # self.play(Write(title))
-        self.add(title)
+        self.play(DrawBorderThenFill(borders))
+        # self.add(borders)
+        self.wait(1)
+        self.play(Write(title))
+        # self.add(title)
         self.wait(2)
