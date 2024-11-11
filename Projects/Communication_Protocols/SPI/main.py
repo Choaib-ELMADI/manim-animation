bbdb133124d3f18194069b3febcc0b1032f31397
Choaib ELMADI <choaibamd@gmail.com>
2024-11-11 13:05:18 +0100

    Add SCLK line animation

--- Projects/Communication_Protocols/SPI/main.py
+++ Projects/Communication_Protocols/SPI/main.py
@@ -2,21 +2,20 @@ from manim import *
 
 
 class Main(Scene):
-
-    def plot_step_function(self, data_bits, axe, color, initial_y, initial_x=0):
-        start_x = initial_x
+    def plot_step_function(self, data_bits, axe, color, initial_y):
+        start_x = 0
         start_y = initial_y
 
         for i, bit in enumerate(data_bits):
-            end_x = initial_x + i + 1
+            end_x = i + 1
             line = Line(
                 axe.c2p(start_x, start_y),
                 axe.c2p(end_x, start_y),
                 color=color,
             )
 
-            self.play(Create(line), run_time=0.25)
-            # self.add(line)
+            # self.play(Create(line), run_time=0.25)
+            self.add(line)
 
             if i < len(data_bits) - 1 and data_bits[i + 1] != bit:
                 transition_line = Line(
@@ -24,8 +23,8 @@ class Main(Scene):
                     axe.c2p(end_x, initial_y - 1 + data_bits[i + 1]),
                     color=color,
                 )
-                self.play(Create(transition_line), run_time=0.1)
-                # self.add(transition_line)
+                # self.play(Create(transition_line), run_time=0.1)
+                self.add(transition_line)
 
             start_x = end_x
             start_y = (
@@ -33,6 +32,67 @@ class Main(Scene):
                 if i < len(data_bits) - 1
                 else initial_y - 1
             )
+
+    def generate_clock_signal(self, length, axe, color, initial_y):
+        start_x = 1
+        start_y = initial_y
+
+        no_clock_line_1 = Line(
+            axe.c2p(0, start_y),
+            axe.c2p(start_x, start_y),
+            color=color,
+        )
+
+        axe.add(no_clock_line_1)
+
+        for i in range(start_x, length + 1):
+            end_x = i + 0.25
+
+            low_state_start = Line(
+                axe.c2p(start_x, start_y),
+                axe.c2p(end_x, start_y),
+                color=color,
+            )
+
+            rising_edge = Line(
+                axe.c2p(end_x, start_y),
+                axe.c2p(end_x, start_y + 1),
+                color=color,
+            )
+
+            high_state = Line(
+                axe.c2p(end_x, start_y + 1),
+                axe.c2p(end_x + 0.5, start_y + 1),
+                color=color,
+            )
+
+            falling_edge = Line(
+                axe.c2p(end_x + 0.5, start_y + 1),
+                axe.c2p(end_x + 0.5, start_y),
+                color=color,
+            )
+
+            low_state_end = Line(
+                axe.c2p(end_x + 0.5, start_y),
+                axe.c2p(end_x + 0.75, start_y),
+                color=color,
+            )
+
+            axe.add(low_state_start)
+            axe.add(rising_edge)
+            axe.add(high_state)
+            axe.add(falling_edge)
+            axe.add(low_state_end)
+
+            start_x = start_x + 1
+
+        no_clock_line_2 = Line(
+            axe.c2p(13, start_y),
+            axe.c2p(14, start_y),
+            color=color,
+        )
+
+        axe.add(no_clock_line_2)
 
     def construct(self):
         cs_line_data = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -103,23 +163,29 @@ class Main(Scene):
         self.add(axe)
         self.wait(1)
 
+        #! CS/SS
         # self.play(Write(cs_line_label))
         self.add(cs_line_label)
         self.wait(0.25)
         self.plot_step_function(cs_line_data, axe, "BLUE", initial_y=7)
 
+        #! SCLK
         # self.play(Write(sclk_line_label))
         self.add(sclk_line_label)
         self.wait(0.25)
+        self.generate_clock_signal(12, axe, "RED", initial_y=4)
 
+        #! MOSI
         # self.play(Write(mosi_line_label))
         self.add(mosi_line_label)
         self.wait(0.25)
 
+        #! MISO
         # self.play(Write(miso_line_label))
         self.add(miso_line_label)
         self.wait(0.25)
 
+        #! END CS/SS
         self.play(Create(cs_transition_line), run_time=0.1)
         # self.add(cs_transition_line)
         self.play(Create(cs_disable_line), run_time=0.25)
