7714cd1e252ea90192eda597af75acbf4c130c49
Choaib ELMADI <choaibamd@gmail.com>
2024-11-12 00:58:29 +0100

    Add SCL and SDA lines animation

---
+++ Projects/Communication_Protocols/I2C/main.py
@@ -0,0 +1,214 @@
+from manim import *
+
+
+class Main(Scene):
+    def plot_step_function(self, data_bits, axe, initial_y):
+        start_x = 0
+        start_y = initial_y + 1
+
+        for i, bit in enumerate(data_bits):
+            custom_color = (
+                "YELLOW"
+                if i == 0 or i == len(data_bits) - 1
+                else (
+                    "GREEN"
+                    if i in [1, 2, 3, 4, 5, 6, 7]
+                    else (
+                        "RED" if i == 8 else ("BLUE" if i == 9 or i == 18 else "ORANGE")
+                    )
+                )
+            )
+
+            #! TRANSMIT DATA
+            end_x = i + 1
+
+            line = Line(
+                axe.c2p(start_x, start_y),
+                axe.c2p(end_x, start_y),
+                color=custom_color,
+            )
+
+            curr_bit_text = (
+                Text(
+                    f"{ bit }",
+                    font="Cascadia Code",
+                    color=custom_color,
+                )
+                .scale(0.5)
+                .next_to(line, UP, buff=0.1)
+            )
+
+            # self.play(Create(line), run_time=0.5)
+            self.add(line)
+            # self.play(Write(curr_bit_text), run_time=0.1)
+            self.add(curr_bit_text)
+
+            #! TRANSITION BETWEEN 1 AND 0
+            if i < len(data_bits) - 1 and data_bits[i + 1] != bit:
+                transition_line = Line(
+                    axe.c2p(end_x, start_y),
+                    axe.c2p(end_x, initial_y + data_bits[i + 1]),
+                    color=custom_color,
+                )
+
+                # self.play(Create(transition_line), run_time=0.25)
+                self.add(transition_line)
+
+            start_x = end_x
+            start_y = (
+                initial_y + data_bits[i + 1] if i < len(data_bits) - 1 else start_y
+            )
+
+    def generate_clock_signal(self, length, axe, color, initial_y):
+        start_x = 1
+        start_y = initial_y
+
+        no_clock_line = Line(
+            axe.c2p(0, start_y),
+            axe.c2p(start_x, start_y),
+            color=color,
+        )
+
+        initial_clock_transition_line = Line(
+            axe.c2p(start_x, start_y),
+            axe.c2p(start_x, start_y - 1),
+            color=color,
+        )
+
+        # self.play(Create(no_clock_line), run_time=0.25)
+        self.add(no_clock_line)
+        # self.play(Create(initial_clock_transition_line), run_time=0.1)
+        self.add(initial_clock_transition_line)
+
+        for i in range(start_x, length + 1):
+            end_x = i + 0.25
+
+            low_state_start = Line(
+                axe.c2p(start_x, start_y - 1),
+                axe.c2p(end_x, start_y - 1),
+                color=color,
+            )
+
+            rising_edge = Line(
+                axe.c2p(end_x, start_y - 1),
+                axe.c2p(end_x, start_y),
+                color=color,
+            )
+
+            high_state = Line(
+                axe.c2p(end_x, start_y),
+                axe.c2p(end_x + 0.5, start_y),
+                color=color,
+            )
+
+            falling_edge = Line(
+                axe.c2p(end_x + 0.5, start_y),
+                axe.c2p(end_x + 0.5, start_y - 1),
+                color=color,
+            )
+
+            low_state_end = Line(
+                axe.c2p(end_x + 0.5, start_y - 1),
+                axe.c2p(end_x + 0.75, start_y - 1),
+                color=color,
+            )
+
+            # self.play(Create(low_state_start), run_time=0.1)
+            self.add(low_state_start)
+            # self.play(Create(rising_edge), run_time=0.25)
+            self.add(rising_edge)
+            # self.play(Create(high_state), run_time=0.1)
+            self.add(high_state)
+            # self.play(Create(falling_edge), run_time=0.25)
+            self.add(falling_edge)
+            # self.play(Create(low_state_end), run_time=0.1)
+            self.add(low_state_end)
+
+            start_x = start_x + 1
+
+        no_clock_line = Line(
+            axe.c2p(19, start_y),
+            axe.c2p(20, start_y),
+            color=color,
+        )
+
+        end_clock_transition_line = Line(
+            axe.c2p(19, start_y - 1),
+            axe.c2p(19, start_y),
+            color=color,
+        )
+
+        # self.play(Create(no_clock_line), run_time=0.25)
+        self.add(no_clock_line)
+        # self.play(Create(end_clock_transition_line), run_time=0.1)
+        self.add(end_clock_transition_line)
+
+    def create_dotted_lines(self, axe, length):
+        for i in range(1, length + 1):
+            dotted_line = DashedLine(
+                axe.c2p(i, 0),
+                axe.c2p(i, 4),
+                dash_length=0.05,
+                dashed_ratio=0.3,
+                color="GREY",
+            )
+            # self.play(Create(dotted_line), run_time=0.1)
+            self.add(dotted_line)
+
+    def construct(self):
+        #           str  add______re_______ss RW  AK  d____a__________t____a  AK stp
+        data_bits = [1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+
+        #! ---- --- -- - ---- --- -- - ---- --- -- - ---- --- -- - !#
+
+        axe = (
+            Axes(
+                x_range=[0, 20, 1],
+                y_range=[0, 4, 1],
+                x_length=35,
+                y_length=8,
+                axis_config={"tip_shape": StealthTip, "tip_length": 0.1},
+                x_axis_config={
+                    "include_numbers": True,
+                },
+            )
+            .scale(0.35)
+            .shift(RIGHT * 0.25)
+        )
+
+        sda_line_label = (
+            Text("SDA", font="Cascadia Code", color="PURPLE")
+            .scale(0.5)
+            .next_to(axe, LEFT, buff=0.1)
+            .align_to(axe, DOWN)
+            .shift(UP * 1.6)
+        )
+
+        scl_line_label = (
+            Text("CSL", font="Cascadia Code", color="RED")
+            .scale(0.5)
+            .next_to(axe, LEFT, buff=0.1)
+            .align_to(axe, DOWN)
+            .shift(UP * 0.2)
+        )
+
+        #! ---- --- -- - ---- --- -- - ---- --- -- - ---- --- -- - !#
+
+        self.wait(0.25)
+        self.add(axe)
+        self.wait(0.25)
+        self.create_dotted_lines(axe, 19)
+        self.wait(0.5)
+
+        # self.play(Write(sda_line_label))
+        self.add(sda_line_label)
+        self.wait(0.25)
+        self.plot_step_function(data_bits, axe, initial_y=2)
+        self.wait(0.25)
+
+        # self.play(Write(scl_line_label))
+        self.add(scl_line_label)
+        self.wait(0.25)
+        self.generate_clock_signal(18, axe, "RED", initial_y=1)
+
+        self.wait(2)
