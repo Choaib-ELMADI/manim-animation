bbdbc197473792ce7a9934fb69b99e0c0d771af0
Choaib ELMADI <choaibamd@gmail.com>
2025-01-27 12:23:33 +0100

    Memory layout section in progress

--- Projects/Processors_Bit_Banding_Feature/main.py
+++ Projects/Processors_Bit_Banding_Feature/main.py
@@ -9,8 +9,9 @@ class Main(Scene):
                 font="Cascadia Code",
                 font_size=54,
                 color="RED",
-            ).scale(0.5)
-            # .to_edge(UP)  #!
+            )
+            .scale(0.5)
+            .to_edge(UP)  #!
         )
 
         topics = Paragraph(
@@ -45,10 +46,8 @@ class Main(Scene):
 
         # ! ---- ---- ---- ---- ----
 
-        # * Definition:
-
-        self.play(Write(title), run_time=1.5)
-        # self.add(title)
+        # self.play(Write(title), run_time=1.5)
+        self.add(title)
         self.play(title.animate.to_edge(UP), run_time=1)  # type: ignore
         self.wait(0.5)
 
@@ -58,25 +57,77 @@ class Main(Scene):
         for i in range(int(topics_length / 2), topics_length - 1):
             topics[i + 1].next_to(topics[i], RIGHT, buff=0.15)
 
-        self.play(Write(topics), run_time=1.5)
-        # self.add(topics)
+        # self.play(Write(topics), run_time=1.5)
+        self.add(topics)
         self.wait(1)
+
+        # * Definition:
 
         topics[0].set_color(YELLOW)
         self.wait(0.5)
 
         for line in bit_banding_def:
-            self.play(Write(line), run_time=1)
-            # self.add(line)
+            # self.play(Write(line), run_time=1)
+            self.add(line)
             self.wait(0.25)
         self.wait(2)
 
         self.play(FadeOut(bit_banding_def, shift=DOWN * 0.5))  # type: ignore
+        self.wait(1)
 
         # * Memory Layout:
 
         topics[0].set_color(WHITE)
         topics[2].set_color(YELLOW)
+        self.wait(0.5)
+
+        memory_areas = [
+            {"name": "Code Area", "size": "0.5 GB", "height": 0.75, "color": YELLOW_C},
+            {"name": "SRAM", "size": "0.5 GB", "height": 0.75, "color": GREEN_A},
+            {"name": "Peripheral", "size": "0.5 GB", "height": 0.75, "color": BLUE},
+            {"name": "External RAM", "size": "1 GB", "height": 0.75, "color": YELLOW_B},
+            {
+                "name": "External Device",
+                "size": "1 GB",
+                "height": 0.75,
+                "color": YELLOW_A,
+            },
+            {
+                "name": "Private Peripheral Bus",
+                "size": "- --",
+                "height": 0.75,
+                "color": GREEN_B,
+            },
+            {
+                "name": "Vendor Specific",
+                "size": "- --",
+                "height": 0.75,
+                "color": PURPLE_B,
+            },
+        ]
+        memory_areas_rectangles = []
+
+        for i, memory_area in enumerate(memory_areas):
+            for j, rect in enumerate(memory_areas_rectangles):
+                if j <= 2 and len(memory_areas_rectangles) <= 3:
+                    rect.shift(DOWN * rect.get_height())
+                if j >= 3:
+                    rect.shift(UP * rect.get_height())
+
+            memory_area_rectangle = Rectangle(
+                color=memory_area["color"],
+                height=memory_area["height"],
+                width=4,
+                fill_opacity=0.5,
+            )
+            memory_area_rectangle.move_to(
+                DOWN * memory_area_rectangle.get_height() * 0.75
+            )
+            self.add(memory_area_rectangle)
+            memory_areas_rectangles.append(memory_area_rectangle)
+            self.wait(0.5)
+
+        self.wait(1)
 
         #! ---- ---- ---- ---- ----
 
