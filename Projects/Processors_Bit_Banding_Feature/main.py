8b0173aff77b7f688b6d7ce23b89c3fa2c8836f9
Choaib ELMADI <choaibamd@gmail.com>
2025-01-27 20:20:33 +0100

    Add some details to memory layout section

--- Projects/Processors_Bit_Banding_Feature/main.py
+++ Projects/Processors_Bit_Banding_Feature/main.py
@@ -85,47 +85,92 @@ class Main(Scene):
             {"name": "Code Area", "size": "0.5 GB", "height": 0.75, "color": YELLOW_C},
             {"name": "SRAM", "size": "0.5 GB", "height": 0.75, "color": GREEN_A},
             {"name": "Peripheral", "size": "0.5 GB", "height": 0.75, "color": BLUE},
-            {"name": "External RAM", "size": "1 GB", "height": 0.75, "color": YELLOW_B},
             {
-                "name": "External Device",
-                "size": "1 GB",
+                "name": "Vendor Specific",
+                "size": "-.- --",
                 "height": 0.75,
-                "color": YELLOW_A,
+                "color": PURPLE_B,
             },
             {
                 "name": "Private Peripheral Bus",
-                "size": "- --",
+                "size": "-.- --",
                 "height": 0.75,
                 "color": GREEN_B,
             },
             {
-                "name": "Vendor Specific",
-                "size": "- --",
+                "name": "External Device",
+                "size": "1.0 GB",
                 "height": 0.75,
-                "color": PURPLE_B,
+                "color": YELLOW_A,
+            },
+            {
+                "name": "External RAM",
+                "size": "1.0 GB",
+                "height": 0.75,
+                "color": YELLOW_B,
             },
         ]
         memory_areas_rectangles = []
+        memory_areas_names = []
+        memory_areas_sizes = []
 
         for i, memory_area in enumerate(memory_areas):
             for j, rect in enumerate(memory_areas_rectangles):
                 if j <= 2 and len(memory_areas_rectangles) <= 3:
+                    # self.play(
+                    #     VGroup(*memory_areas_rectangles[0 : min(2, len(memory_areas_rectangles)) + 1], *memory_areas_names[0 : min(2, len(memory_areas_rectangles)) + 1], *memory_areas_sizes[0 : min(2, len(memory_areas_rectangles)) + 1]).animate.shift(  # type: ignore
+                    #         DOWN * rect.get_height()
+                    #     )
+                    # )
+                    # break
                     rect.shift(DOWN * rect.get_height())
+                    memory_areas_names[j].shift(DOWN * rect.get_height())
+                    memory_areas_sizes[j].shift(DOWN * rect.get_height())
                 if j >= 3:
+                    # self.play(
+                    #     VGroup(*memory_areas_rectangles[3 : min(5, len(memory_areas_rectangles)) + 1], *memory_areas_names[3 : min(5, len(memory_areas_rectangles)) + 1], *memory_areas_sizes[3 : min(5, len(memory_areas_rectangles)) + 1]).animate.shift(  # type: ignore
+                    #         UP * rect.get_height()
+                    #     )
+                    # )
+                    # break
                     rect.shift(UP * rect.get_height())
+                    memory_areas_names[j].shift(UP * rect.get_height())
+                    memory_areas_sizes[j].shift(UP * rect.get_height())
 
             memory_area_rectangle = Rectangle(
                 color=memory_area["color"],
                 height=memory_area["height"],
                 width=4,
-                fill_opacity=0.5,
+                fill_opacity=0.1,
             )
             memory_area_rectangle.move_to(
                 DOWN * memory_area_rectangle.get_height() * 0.75
             )
+            # self.play(DrawBorderThenFill(memory_area_rectangle), run_time=1.5)
             self.add(memory_area_rectangle)
             memory_areas_rectangles.append(memory_area_rectangle)
-            self.wait(0.5)
+
+            memory_area_name = Text(
+                memory_area["name"],
+                font="Cascadia Code",
+                font_size=16,
+                color=memory_area["color"],
+            ).move_to(memory_area_rectangle.get_center())
+            # self.play(Write(memory_area_name))
+            self.add(memory_area_name)
+            memory_areas_names.append(memory_area_name)
+
+            memory_area_size = Text(
+                memory_area["size"],
+                font="Cascadia Code",
+                font_size=16,
+                color=memory_area["color"],
+            ).next_to(memory_area_rectangle, RIGHT, buff=0.25)
+            # self.play(Write(memory_area_size))
+            self.add(memory_area_size)
+            memory_areas_sizes.append(memory_area_size)
+
+            self.wait(0.25)
 
         self.wait(1)
 
